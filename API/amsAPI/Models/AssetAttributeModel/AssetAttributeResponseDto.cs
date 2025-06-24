18924544c00d4041f0bc0a9fdb2d9101e90e8c02
Exaze TRSA27 <Exaze TRSA27@EXZ_MP2HMP1P>
2025-06-24 11:31:51 +0200

    Feature-GetAllAssets and GetById

---
+++ API/amsAPI/Models/AssetAttributeModel/AssetAttributeResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.FeatureModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace amsAPI.Models.AssetAttributeModel
+{
+    public class AssetAttributeResponseDto
+    {
+        [Key]
+        public Guid AssetAttributeId { get; set; }
+        [Required]
+        public FeatureDto Feature { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Value { get; set; }
+    }
+}
---
+++ API/amsAPI/Models/AssetModel/AssetResponseDto.cs
@@ -0,0 +1,25 @@
+﻿using amsAPI.Models.AssetAttributeModel;
+using Domain.Models.AssetAttributeModel;
+using Domain.Models.BrandModel;
+using Domain.Models.CategoryModel;
+using Domain.Models.LocationModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace amsAPI.Models.AssetModel
+{
+    public class AssetResponseDto
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public CategoryDto Category { get; set; }
+        public LocationDto Location { get; set; }
+        public BrandDto Brand { get; set; }
+
+        public string SerialNumber { get; set; }
+        public string Description { get; set; }
+        public bool IsAssigned { get; set; }
+
+        public List<AssetAttributeResponseDto> AssetAttributes { get; set; }
+    }
+}
